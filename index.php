98992ccadee760c97a1c0ce38d30903bbfeb541a
gadibh53 <gadibh53@gmail.com>
2019-12-21 12:20:57 +0200

    indentation

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>test1</title>
+  <title>test1</title>
 </head>
 <body>
-	<div>
-		<h1>Hello World!</h1>
-	</div>
+  <div>
+    <h1>Hello World!</h1>
+  </div>
 </body>
 </html>
